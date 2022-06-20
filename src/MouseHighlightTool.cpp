89a8827d5653e2bf095d7e14f1bfd27f581e2495
AnatolyRybchych <tol.ryb@gmail.com>
2022-06-20 18:25:09 +0300

    better contrast setting for MouseHighlightTool

--- src/MouseHighlightTool.cpp
+++ src/MouseHighlightTool.cpp
@@ -9,7 +9,7 @@ MouseHighlightTool::MouseHighlightTool(int cx, int cy) noexcept
 }
 
 void MouseHighlightTool::OnDraw() const noexcept{
-    MouseHighlight::GetRenderer().Draw(mX * 2.0 / GetViewportWidth() - 1.0 ,1.0 - mY * 2.0 / GetViewportHeight(), 0, 0, 0, 0.5, 1.0 / radius, radius);
+    MouseHighlight::GetRenderer().Draw(mX * 2.0 / GetViewportWidth() - 1.0 ,1.0 - mY * 2.0 / GetViewportHeight(), 0, 0, 0,1.0 - radius  / 2, 1.0 / radius, radius);
 }
 
 void MouseHighlightTool::OnResize(int cx, int cy) noexcept{}
@@ -29,10 +29,7 @@ void MouseHighlightTool::OnLMouseUp(int x, int y) noexcept{
     mY = y;
 }
 
-void MouseHighlightTool::OnKeyDown(int vkCode, int repeat) noexcept{
-    std::cout<<vkCode<<"\n";
-}
-
+void MouseHighlightTool::OnKeyDown(int vkCode, int repeat) noexcept{}
 void MouseHighlightTool::OnKeyUp(int vkCode) noexcept{}
 void MouseHighlightTool::OnTextInput(std::wstring str) noexcept{}
 
